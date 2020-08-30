feeb8141c2fec7c64116332c0e48b9e25d48ca24
Sean <skelstar@gmail.com>
2020-08-30 15:37:44 +1200

    Got patterns working. Timing is right (enough).

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -16,13 +16,73 @@
 
 CRGB leds[NUM_LEDS];
 
-void setLeds(CRGB colour)
+void setLed(CRGB colour, int i, bool show = true)
+{
+  leds[i] = colour;
+  if (show)
+    FastLED.show();
+}
+
+void setLeds(CRGB colour, bool show = true)
 {
   for (int num = 0; num < NUM_LEDS; num++)
   {
     leds[num] = colour;
   }
-  FastLED.show();
+  if (show)
+    FastLED.show();
+}
+
+bool isIn(int *data, int len, int idx)
+{
+  int buff[len];
+  memcpy(buff, data, len);
+  for (int i = 0; i < len; i++)
+  {
+    if (buff[i] == idx)
+    {
+      Serial.printf("X");
+      return true;
+    }
+  }
+  Serial.printf("-");
+  return false;
+}
+
+int ring0[] = {12};
+int ring1[] = {6, 7, 8, 11, 13, 16, 17, 18};
+int ring2[] = {0, 1, 2, 3, 4, 5, 9, 10, 14, 15, 19, 20, 21, 22, 23, 24};
+
+void setRing(CRGB colour, int ring)
+{
+  switch (ring)
+  {
+  case 1:
+  {
+    setLeds(CRGB::Black, false);
+    leds[12] = colour;
+    FastLED.show();
+    break;
+  }
+
+  case 2:
+  {
+    setLeds(CRGB::Black, false);
+    for (int i = 0; i < sizeof(ring1) / sizeof(ring1[0]); i++)
+      leds[ring1[i]] = colour;
+    FastLED.show();
+    break;
+  }
+
+  case 3:
+  {
+    setLeds(CRGB::Black, false);
+    for (int i = 0; i < sizeof(ring2) / sizeof(ring2[0]); i++)
+      leds[ring2[i]] = colour;
+    FastLED.show();
+    break;
+  }
+  }
 }
 
 //--------------------------------------------
@@ -40,6 +100,14 @@ Fsm *lightsFsm;
 
 //------------------------------------------
 
+#define BLOOM_TIME_MS 30 * 1000
+#define FILL_AND_STIR_MS 90 * 1000
+#define END_TIME_MS 10 * 1000
+
+// #define BLOOM_TIME_SECONDS 3
+// #define FILL_AND_STIR_SECONDS 3
+// #define END_TIME 3
+
 elapsedMillis sinceNextState = 0;
 
 State stateStart(
@@ -49,18 +117,18 @@ State stateStart(
       FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
       FastLED.setBrightness(30);
 
-      setLeds(CRGB::Black);
+      setRing(CRGB::Green, 1);
       sinceNextState = 0;
     });
 
 State stateBloom(
     [] {
       Serial.printf("State: stateBloom\n");
+      setRing(CRGB::Blue, 2);
       sinceNextState = 0;
-      setLeds(CRGB::Blue);
     },
     [] {
-      if (sinceNextState > 2000)
+      if (sinceNextState > BLOOM_TIME_MS)
       {
         lightsFsm->trigger(EV_NEXT);
       }
@@ -72,10 +140,10 @@ State stateFillAndStir(
     [] {
       Serial.printf("State: stateFillAndStir\n");
       sinceNextState = 0;
-      setLeds(CRGB::Green);
+      setRing(CRGB::Green, 3);
     },
     [] {
-      if (sinceNextState > 5000)
+      if (sinceNextState > FILL_AND_STIR_MS)
       {
         lightsFsm->trigger(EV_NEXT);
       }
@@ -90,7 +158,7 @@ State stateEnd(
       setLeds(CRGB::Red);
     },
     [] {
-      if (sinceNextState > 2000)
+      if (sinceNextState > END_TIME_MS)
       {
         lightsFsm->trigger(EV_NEXT);
       }
